84afd3b1ad143b6af7fae22893008b6647fc483d
pavlik1l <pavlik1l@ya.ru>
2020-09-21 22:27:30 +0300

    menu

--- header.php
+++ header.php
@@ -876,15 +876,139 @@
             <li>
             <a href="#">Бизиборды</a>
             <ul class="sub-menu">
-              <li><a href="#">Смотреть все бизиборды</a></li>
+              <li><a href="#">
+                <img src="https://xn--80ablmoh8a2h.xn--p1ai/galery/miniature/min-B260.1.jpg" alt=""> 
+                <span>Смотреть все бизиборды</span>
+               </a>
+              </li>
+              <li><a href="#">
+                <img src="https://xn--80ablmoh8a2h.xn--p1ai/galery/miniature/min-B260.1.jpg" alt=""> 
+                <span>Машинки</span>
+               </a>
+              </li>
+              <li><a href="#">
+                <img src="https://xn--80ablmoh8a2h.xn--p1ai/galery/miniature/min-B260.1.jpg" alt=""> 
+                <span>Бизидом</span>
+               </a>
+              </li>
+              <li><a href="#">
+                <img src="https://xn--80ablmoh8a2h.xn--p1ai/galery/miniature/min-B260.1.jpg" alt=""> 
+                <span>В дорогу</span>
+               </a>
+              </li>
+              <li><a href="#">
+                <img src="https://xn--80ablmoh8a2h.xn--p1ai/galery/miniature/min-B260.1.jpg" alt=""> 
+                <span>Бизикуб</span>
+               </a>
+              </li>
+              <li><a href="#">
+                <img src="https://xn--80ablmoh8a2h.xn--p1ai/galery/miniature/min-B260.1.jpg" alt=""> 
+                <span>Для детских садов и ДОУ</span>
+               </a>
+              </li>
+              <li><a href="#">
+                <img src="https://xn--80ablmoh8a2h.xn--p1ai/galery/miniature/min-B260.1.jpg" alt=""> 
+                <span>Классические</span>
+               </a>
+              </li>
+              <li><a href="#">
+                <img src="https://xn--80ablmoh8a2h.xn--p1ai/galery/miniature/min-B260.1.jpg" alt=""> 
+                <span>Бизиборды-малютки</span>
+               </a>
+              </li>
             </ul>
             </li>
-            <li><a href="#">Авторские методики развития</a></li>
+            <li><a href="#">Авторские методики развития</a>
+          
+            <ul class="sub-menu">
+              <li><a href="#">
+                <img src="https://xn--80ablmoh8a2h.xn--p1ai/galery/miniature/min-B260.1.jpg" alt=""> 
+                <span>Все методики</span>
+               </a>
+              </li>
+              <li><a href="#">
+                <img src="https://xn--80ablmoh8a2h.xn--p1ai/galery/miniature/min-B260.1.jpg" alt=""> 
+                <span>Машинки</span>
+               </a>
+              </li>
+              <li><a href="#">
+                <img src="https://xn--80ablmoh8a2h.xn--p1ai/galery/miniature/min-B260.1.jpg" alt=""> 
+                <span>Бизидом</span>
+               </a>
+              </li>
+              <li><a href="#">
+                <img src="https://xn--80ablmoh8a2h.xn--p1ai/galery/miniature/min-B260.1.jpg" alt=""> 
+                <span>В дорогу</span>
+               </a>
+              </li>
+              <li><a href="#">
+                <img src="https://xn--80ablmoh8a2h.xn--p1ai/galery/miniature/min-B260.1.jpg" alt=""> 
+                <span>Бизикуб</span>
+               </a>
+              </li>
+              <li><a href="#">
+                <img src="https://xn--80ablmoh8a2h.xn--p1ai/galery/miniature/min-B260.1.jpg" alt=""> 
+                <span>Для детских садов и ДОУ</span>
+               </a>
+              </li>
+              <li><a href="#">
+                <img src="https://xn--80ablmoh8a2h.xn--p1ai/galery/miniature/min-B260.1.jpg" alt=""> 
+                <span>Классические</span>
+               </a>
+              </li>
+              <li><a href="#">
+                <img src="https://xn--80ablmoh8a2h.xn--p1ai/galery/miniature/min-B260.1.jpg" alt=""> 
+                <span>Бизиборды-малютки</span>
+               </a>
+              </li>
+            </ul>
+          </li>
             <li><a href="#">Мелкая моторика</a></li>
             <li><a href="#">Магнитные конструкторы</a></li>
             <li><a href="#">Музыкальные игрушки</a></li>
             <li><a href="#">Книги</a></li>
-            <li><a href="#">Коврики и пазлы</a></li>
+            <li><a href="#">Коврики и пазлы</a><ul class="sub-menu">
+              <li><a href="#">
+                <img src="https://xn--80ablmoh8a2h.xn--p1ai/galery/miniature/min-B260.1.jpg" alt=""> 
+                <span>Все коврики</span>
+               </a>
+              </li>
+              <li><a href="#">
+                <img src="https://xn--80ablmoh8a2h.xn--p1ai/galery/miniature/min-B260.1.jpg" alt=""> 
+                <span>Машинки</span>
+               </a>
+              </li>
+              <li><a href="#">
+                <img src="https://xn--80ablmoh8a2h.xn--p1ai/galery/miniature/min-B260.1.jpg" alt=""> 
+                <span>Бизидом</span>
+               </a>
+              </li>
+              <li><a href="#">
+                <img src="https://xn--80ablmoh8a2h.xn--p1ai/galery/miniature/min-B260.1.jpg" alt=""> 
+                <span>В дорогу</span>
+               </a>
+              </li>
+              <li><a href="#">
+                <img src="https://xn--80ablmoh8a2h.xn--p1ai/galery/miniature/min-B260.1.jpg" alt=""> 
+                <span>Бизикуб</span>
+               </a>
+              </li>
+              <li><a href="#">
+                <img src="https://xn--80ablmoh8a2h.xn--p1ai/galery/miniature/min-B260.1.jpg" alt=""> 
+                <span>Для детских садов и ДОУ</span>
+               </a>
+              </li>
+              <li><a href="#">
+                <img src="https://xn--80ablmoh8a2h.xn--p1ai/galery/miniature/min-B260.1.jpg" alt=""> 
+                <span>Классические</span>
+               </a>
+              </li>
+              <li><a href="#">
+                <img src="https://xn--80ablmoh8a2h.xn--p1ai/galery/miniature/min-B260.1.jpg" alt=""> 
+                <span>Бизиборды-малютки</span>
+               </a>
+              </li>
+            </ul></li>
             <li><a href="#">Блочный конструктор</a></li>
             <li><a href="#">Все для творчества</a></li>
             <li><a href="#">Сюжетные и ролевые игры</a></li>
