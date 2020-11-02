f2f14a07fac8f3d2f1fc73be0b9b834f5e7d2c57
asmi046 <asmi046@gmail.com>
2020-11-03 01:18:11 +0300

    Убрал из поиска битые

--- search.php
+++ search.php
@@ -16,6 +16,64 @@
 				if( $detect->isMobile() )
 					//include("search-form.php");
 			?>
+			<?
+				global $query_string;
+				parse_str($query_string, $args);	
+				
+				$args = array_diff($args, [$_REQUEST["s"]]);
+
+				$metaquery = array(
+					'relation' => 'AND',
+					'orderCatM' => array (
+						'key'     => '_tovar_order',
+						'compare' => 'EXIST',
+						'type'    => 'NUMERIC',
+					),
+					
+					'priceM' => array (
+						'key'     => 'price',
+						'value' => '0',
+						'compare' => '>',
+						'type'    => 'NUMERIC',
+					),
+				
+					'predzak' => array (
+						'key'     => '_sclad_count',
+						'value' => '0',
+						'compare' => '>',
+						'type'    => 'NUMERIC',
+					),
+				
+				
+				);
+				
+				
+				$orderby = ($_REQUEST["sortparam"] === "price")?"priceM":"orderCatM";  
+				$metakey = empty($_REQUEST["sortparam"])?"_tovar_order":$_REQUEST["sortparam"];  
+				$order = empty($_REQUEST["ordn"])?"ASC":$_REQUEST["ordn"];  
+				
+				
+		
+				$args['meta_query'] = $metaquery;
+				$args['orderby'] = $orderby;
+				$args['order'] = $order;	
+				
+				$search_term = $_REQUEST["s"];	
+				
+				add_filter( 'posts_where', 'title_like_posts_where', 10, 2 );
+				function title_like_posts_where( $where, $wp_query ) {
+					global $wpdb;
+					if ( $post_title_like = $wp_query->get( 'post_title_like' ) ) {
+						$where .= ' AND ' . $wpdb->posts . '.post_title LIKE \'%' . esc_sql( $wpdb->esc_like( $post_title_like ) ) . '%\'';
+					}
+					return $where;
+				}
+
+				$args['post_title_like'] = $search_term;
+				
+				
+				query_posts( $args );
+			?>
 			
 				<?php if ( have_posts() ) : ?>
 					<h1>Результаты поиска по запросу: <?php echo $_REQUEST["s"];?></h1>
