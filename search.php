1193ad012453bc3e90a0dd66e1c90aa4d1f4dd18
asmi046 <asmi046@gmail.com>
2020-11-04 15:09:23 +0300

    Поиск по почетанию слов

--- search.php
+++ search.php
@@ -60,11 +60,18 @@
 				
 				$search_term = $_REQUEST["s"];	
 				
+
 				add_filter( 'posts_where', 'title_like_posts_where', 10, 2 );
 				function title_like_posts_where( $where, $wp_query ) {
 					global $wpdb;
 					if ( $post_title_like = $wp_query->get( 'post_title_like' ) ) {
-						$where .= ' AND ' . $wpdb->posts . '.post_title LIKE \'%' . esc_sql( $wpdb->esc_like( $post_title_like ) ) . '%\'';
+						//$where .= ' AND ' . $wpdb->posts . '.post_title LIKE \'%' . esc_sql( $wpdb->esc_like( $post_title_like ) ) . '%\'';
+						$argument = explode(" ", $post_title_like);
+						for ($i =0; $i<count($argument); $i++)
+							$where .= ' AND ' . $wpdb->posts . '.post_title LIKE \'%' . trim($argument[$i]) . '%\'';
+						//$where .= ' AND ' . $wpdb->posts . '.post_title LIKE \'%' . "Развивайка" . '%\'';
+						//$where .= ' AND ' . $wpdb->posts . '.post_title LIKE \'%' . "Большая" . '%\'';
+						
 					}
 					return $where;
 				}
